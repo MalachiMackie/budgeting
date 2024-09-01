3cf10f4c05dc51258f6aee7daca66b74e82826e4
Malachi Mackie <malachi@mackie.co.nz>
2024-09-02 11:43:18 +1200

    http request tracing

--- src/main.rs
+++ src/main.rs
@@ -27,30 +27,9 @@ async fn main() {
                         "http_request",
                         method = ?request.method(),
                         matched_path,
-                        some_other_field = tracing::field::Empty,
+                        // some_other_field = tracing::field::Empty,
                     )
-                })
-                .on_request(|_request: &Request<_>, _span: &Span| {
-                    // You can use `_span.record("some_other_field", value)` in one of these
-                    // closures to attach a value to the initially empty field in the info_span
-                    // created above.
-                })
-                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
-                    // ...
-                })
-                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
-                    // ...
-                })
-                .on_eos(
-                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
-                        // ...
-                    },
-                )
-                .on_failure(
-                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
-                        // ...
-                    },
-                ),);
+                }));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -65,18 +44,18 @@ async fn hello_world() -> &'static str {
 }
 
 fn init_logger() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
                 // axum logs rejections from built-in extractors with the `axum::rejection`
                 // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
                 format!(
-                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    "{}=debug,tower_http=trace,axum::rejection=trace",
                     env!("CARGO_CRATE_NAME")
                 )
                 .into()
-            }),
-        )
+            });
+
+    tracing_subscriber::registry()
+        .with(env_filter)
         .with(tracing_subscriber::fmt::layer())
         .init();
 }
