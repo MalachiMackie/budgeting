becac606c9a6638935d0edcdbf281ebefa1aff45
Malachi Mackie <malachi@mackie.co.nz>
2024-09-07 08:17:34 +1200

    Add new transaction ui

--- budgeting-ui/src/views/transactionList.tsx
+++ budgeting-ui/src/views/transactionList.tsx
@@ -1,4 +1,4 @@
-import { DataTableSkeleton, Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@carbon/react";
+import { Button, DataTableSkeleton, DatePicker, DatePickerInput, Dropdown, NumberInput, Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@carbon/react";
 import { useEffect, useState } from "react"
 
 type Transaction = {
@@ -19,6 +19,8 @@ export default function TransactionList(): JSX.Element {
     const [payees, setPayees] = useState<Payee[]>([]);
     const [loadingState, setLoading] = useState<{transactions: boolean; payees: boolean}>({transactions: false, payees: false});
     const loading = loadingState.transactions || loadingState.payees;
+    const [refreshCount, setRefreshCount] = useState(0);
+    const refresh = () => setRefreshCount(x => x + 1);
 
     useEffect(() => {
         let loadTransactions = async () => {
@@ -38,7 +40,7 @@ export default function TransactionList(): JSX.Element {
 
         void loadTransactions();
         void loadPayees();
-    }, []);
+    }, [refreshCount]);
 
     let payeesMap = new Map(payees.map(x => [x.id, x]))
 
@@ -48,6 +50,15 @@ export default function TransactionList(): JSX.Element {
 
     const headers  =[{key: 'time', header: "Time"}, {key: 'amount', header: "Amount"}, {key: 'payee', header: 'Payee'}];
 
+    const saveTransaction = async (x: CreateTransactionRequest) => {
+        await fetch("http://localhost:3000/api/transactions", {method: 'POST', body: JSON.stringify(x), headers: {"Content-Type": "application/json"}});
+    }
+
+    const sortTransactions = (a: Transaction, b: Transaction): number => {
+        // todo: within the same day, compare by amount
+        return a.time.localeCompare(b.time);
+    }
+
     return <Table size="sm">
         <TableHead>
             <TableRow>
@@ -55,11 +66,71 @@ export default function TransactionList(): JSX.Element {
             </TableRow>
         </TableHead>
         <TableBody>
-            {transactions.map(x => <TableRow key={x.id}>
+            <NewTransactionRow onComplete={() => refresh()} save={saveTransaction} payees={payees} />
+            {[...transactions].sort(sortTransactions).map(x => <TableRow key={x.id}>
                 <TableCell>{new Date(x.time).toLocaleDateString()}</TableCell>
                 <TableCell>${(x.amount_dollars + ((x.amount_cents * Math.sign(x.amount_dollars)) / 100)).toFixed(2)}</TableCell>
                 <TableCell>{payeesMap.get(x.payee_id)!.name}</TableCell>
             </TableRow>)}
         </TableBody>
     </Table>
+}
+
+type CreateTransactionRequest = {
+    payee_id: string,
+    amount_dollars: number,
+    amount_cents: number,
+    time: string
+};
+
+function NewTransactionRow({payees, save, onComplete}: {payees: Payee[], save: (x: CreateTransactionRequest) => Promise<void>, onComplete: () => void}): JSX.Element {
+    const [date, setDate] = useState(new Date());
+    const [amount, setAmount] = useState<number | undefined>(undefined);
+    const [payeeId, setPayeeId] = useState<string | undefined>(undefined)
+    const handleSaveClick = async () => {
+        if (!payeeId) {
+            alert("Cannot save transaction without a payee");
+            return;
+        }
+        if (typeof amount === "undefined") {
+            alert("Cannot save transaction without a transaction amount");
+            return;
+        }
+        // todo: do this better maybe?
+        const amountDollars = Math.trunc(amount);
+        const amountCents = Math.trunc(Math.abs((amount - amountDollars)) * 100);
+        try {
+            await save({
+                time: date.toISOString(),
+                payee_id: payeeId,
+                amount_dollars: amountDollars,
+                amount_cents: amountCents
+            });
+            onComplete();
+            setDate(new Date());
+            setAmount(undefined);
+            setPayeeId(undefined);
+        } catch {
+            // handle error
+        }
+    }
+
+    const dropdownItems = payees.map(x => ({id: x.id, value: x.id, label: x.name}));
+
+    return <TableRow>
+        <TableCell>
+            <DatePicker onChange={x => x.length == 1 && setDate(x[0])} value={date} datePickerType="single" >
+                <DatePickerInput size="sm" id={"date"} labelText={undefined} />
+            </DatePicker>
+        </TableCell>
+        <TableCell>
+            <NumberInput value={amount} onChange={(_, {value}) => typeof value === "number" && setAmount(value)} size="sm" id={"amount"} />
+        </TableCell>
+        <TableCell>
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <Dropdown onChange={x => x.selectedItem && setPayeeId(x.selectedItem.id)} initialSelectedItem={dropdownItems.find(x => x.id === payeeId)} itemToString={x => x?.label ?? ""} size="sm" id={"payee"} items={dropdownItems} label={""} />
+                <Button onClick={handleSaveClick}>Save</Button>
+            </div>
+        </TableCell>
+    </TableRow>;
 }
